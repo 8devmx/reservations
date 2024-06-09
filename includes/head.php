bf7648b4e8d8a5df0c3fe362c621c87d3f12f622
Ramiro440 <ramirohaces44@gmail.com>
2024-06-09 16:49:20 -0600

    Filtro completo
    Me falto esta parte, para que salga desplegado el filtro

--- includes/head.php
+++ includes/head.php
@@ -6,4 +6,7 @@
 <title>Modelos de Bases de datos</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
+<link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.min.css" rel="stylesheet">
