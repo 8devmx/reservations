1ec44f33df2b3785e9fdb3ba65d48a880e282402
CARLOS:TORRES <93115180+01ARTURO01@users.noreply.github.com>
2024-06-20 01:02:31 -0500

    sidebar
    correcion de la clase hoy 19 de junio 2024

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,19 +1,35 @@
-<div class="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary">
-  <ul>
-    <li>
-      <a href="#">Dashboard</a>
+
+<div class="sidebar border border-right col-md-3 col-lg-2 p-0 bg-dark text-white ">
+  <ul class="nav flex-column">
+    <li class="nav-item">
+      <a href="#" class="nav-link text-white d-flex ">
+        <div class="me-2" style=" height: 10vh; margin-right: 1.5rem !important;"><i class="bi bi-speedometer2"></i></div>
+        <p>Dashboard</p>
+      </a>
     </li>
-    <li>
-      <a href="http://localhost/modelos-de-bd-primer-parcial/modules/users/index.php">Usuarios</a>
+    <li class="nav-item">
+      <a href="http://localhost/modelos-de-bd-primer-parcial/modules/users/index.php" class="nav-link text-white d-flex ">
+        <div class="me-2" style=" height: 10vh; margin-right: 1.5rem !important;"><i class="bi bi-people"></i></div>
+        <p>Usuarios</p>
+      </a>
     </li>
-    <li>
-      <a href="http://localhost/modelos-de-bd-primer-parcial/modules/clients/index.php">Clientes</a>
+    <li class="nav-item">
+      <a href="http://localhost/modelos-de-bd-primer-parcial/modules/clients/index.php" class="nav-link text-white d-flex ">
+        <div class="me-2" style=" height: 10vh; margin-right: 1.5rem !important;"><i class="bi bi-person"></i></div>
+        <p>Clientes</p>
+      </a>
     </li>
-    <li>
-      <a href="http://localhost/modelos-de-bd-primer-parcial/modules/roles/index.php">Roles</a>
+    <li class="nav-item">
+      <a href="http://localhost/modelos-de-bd-primer-parcial/modules/roles/index.php" class="nav-link text-white d-flex ">
+        <div class="me-2" style=" height: 10vh; margin-right: 1.5rem !important;"><i class="bi bi-person-badge"></i></div>
+        <p>Roles</p>
+      </a>
     </li>
-    <li>
-      <a href="http://localhost/modelos-de-bd-primer-parcial/modules/events/index.php">Reservaciones</a>
+    <li class="nav-item">
+      <a href="http://localhost/modelos-de-bd-primer-parcial/modules/events/index.php" class="nav-link text-white d-flex ">
+        <div class="me-2" style=" height: 10vh; margin-right: 1.5rem !important;"><i class="bi bi-calendar-event"></i></div>
+        <p>Reservaciones</p>
+      </a>
     </li>
   </ul>
 </div>
