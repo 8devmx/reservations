dd610184d7ffd4a382d462739e7c9c0374586406
CARLOS:TORRES <93115180+01ARTURO01@users.noreply.github.com>
2024-06-26 22:11:16 -0500

    errores-sidebar

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,37 +1,38 @@
 <div class="sidebar border border-right col-md-3 col-lg-2 p-0 bg-dark text-white" style="height: 120vh;">
   <ul class="nav flex-column">
     <li class="nav-item">
-      <a href="#" class="nav-link text-white d-flex active">
+      <a href="#" class="nav-link text-white d-flex align-items-center active">
         <div class="icon me-2"><i class="bi bi-speedometer2"></i></div>
-        <p>Dashboard</p>
+        <span>Dashboard</span>
       </a>
     </li>
     <li class="nav-item">
-      <a href="http://localhost/reservations/modules/users/index.php" class="nav-link text-white d-flex">
+      <a href="http://localhost/reservations/modules/users/index.php" class="nav-link text-white d-flex align-items-center">
         <div class="icon me-2"><i class="bi bi-person"></i></div>
-        <p>Usuarios</p>
+        <span>Usuarios</span>
       </a>
     </li>
     <li class="nav-item">
-      <a href="http://localhost/reservations/modules/clients/index.php" class="nav-link text-white d-flex">
+      <a href="http://localhost/reservations/modules/clients/index.php" class="nav-link text-white d-flex align-items-center">
         <div class="icon me-2"><i class="bi bi-people"></i></div>
-        <p>Clientes</p>
+        <span>Clientes</span>
       </a>
     </li>
     <li class="nav-item">
-      <a href="http://localhost/reservations/modules/roles/index.php" class="nav-link text-white d-flex">
+      <a href="http://localhost/reservations/modules/roles/index.php" class="nav-link text-white d-flex align-items-center">
         <div class="icon me-2"><i class="bi bi-key"></i></div>
-        <p>Roles</p>
+        <span>Roles</span>
       </a>
     </li>
     <li class="nav-item">
-      <a href="http://localhost/reservations/modules/events/index.php" class="nav-link text-white d-flex">
+      <a href="http://localhost/reservations/modules/events/index.php" class="nav-link text-white d-flex align-items-center">
         <div class="icon me-2"><i class="bi bi-calendar-event"></i></div>
-        <p>Reservaciones</p>
+        <span>Reservaciones</span>
       </a>
     </li>
   </ul>
 </div>
+
 
 
 
