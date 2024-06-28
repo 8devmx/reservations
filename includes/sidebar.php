4edd7fe26881e43b28dbd2b814993e9c7751187b
CARLOS:TORRES <93115180+01ARTURO01@users.noreply.github.com>
2024-06-28 02:53:01 -0500

    side-bar 4

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,33 +1,33 @@
-<div class="new-sidebar border border-right col-md-3 col-lg-2 p-0 bg-secondary text-light" style="height: 120vh;">
+<div class="sidebar border border-right col-md-3 col-lg-2 p-0 bg-dark text-white" style="height: 120vh;">
   <ul class="nav flex-column">
     <li class="nav-item">
-      <a href="#" class="nav-link text-light d-flex align-items-center active">
+      <a href="#" class="nav-link text-white d-flex active">
         <div class="icon me-2"><i class="bi bi-speedometer2"></i></div>
-        <span>Dashboard</span>
+        <p>Dashboard</p>
       </a>
     </li>
     <li class="nav-item">
-      <a href="http://localhost/reservations/modules/projects/index.php" class="nav-link text-light d-flex align-items-center">
+      <a href="/modelos-de-bd-primer-parcial/modules/users/" class="nav-link text-white d-flex">
         <div class="icon me-2"><i class="bi bi-person"></i></div>
-        <span>Usuarios</span>
+        <p>Usuarios</p>
       </a>
     </li>
     <li class="nav-item">
-      <a href="http://localhost/reservations/modules/tasks/index.php" class="nav-link text-light d-flex align-items-center">
+      <a href="/modelos-de-bd-primer-parcial/modules/clients/" class="nav-link text-white d-flex">
         <div class="icon me-2"><i class="bi bi-people"></i></div>
-        <span>Clientes</span>
+        <p>Clientes</p>
       </a>
     </li>
     <li class="nav-item">
-      <a href="http://localhost/reservations/modules/reports/index.php" class="nav-link text-light d-flex align-items-center">
+      <a href="/modelos-de-bd-primer-parcial/modules/roles/" class="nav-link text-white d-flex">
         <div class="icon me-2"><i class="bi bi-key"></i></div>
-        <span>Roles</span>
+        <p>Roles</p>
       </a>
     </li>
     <li class="nav-item">
-      <a href="http://localhost/reservations/modules/settings/index.php" class="nav-link text-light d-flex align-items-center">
+      <a href="/modelos-de-bd-primer-parcial/modules/events/" class="nav-link text-white d-flex">
         <div class="icon me-2"><i class="bi bi-calendar-event"></i></div>
-        <span>Reservaciones</span>
+        <p>Reservaciones</p>
       </a>
     </li>
   </ul>
