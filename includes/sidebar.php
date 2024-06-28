6d4eee3e4500f1c153bc5869b88421a526311b04
CARLOS:TORRES <93115180+01ARTURO01@users.noreply.github.com>
2024-06-28 03:13:04 -0500

    Update sidebar.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,1 +1,35 @@
+<div class="sidebar border border-right col-md-3 col-lg-2 p-0 bg-dark text-white" style="height: 120vh;">
+  <ul class="nav flex-column">
+    <li class="nav-item">
+      <a href="#" class="nav-link text-white d-flex active">
+        <div class="icon me-2"><i class="bi bi-speedometer2"></i></div>
+        <p>Dashboard</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="/modelos-de-bd-primer-parcial/modules/users/" class="nav-link text-white d-flex">
+        <div class="icon me-2"><i class="bi bi-person"></i></div>
+        <p>Usuarios</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="/modelos-de-bd-primer-parcial/modules/clients/" class="nav-link text-white d-flex">
+        <div class="icon me-2"><i class="bi bi-people"></i></div>
+        <p>Clientes</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="/modelos-de-bd-primer-parcial/modules/roles/" class="nav-link text-white d-flex">
+        <div class="icon me-2"><i class="bi bi-key"></i></div>
+        <p>Roles</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="/modelos-de-bd-primer-parcial/modules/events/" class="nav-link text-white d-flex">
+        <div class="icon me-2"><i class="bi bi-calendar-event"></i></div>
+        <p>Reservaciones</p>
+      </a>
+    </li>
+  </ul>
+</div>
 
