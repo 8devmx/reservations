30e5796c850e467cb758c726683c8ce5ce009700
CARLOS:TORRES <93115180+01ARTURO01@users.noreply.github.com>
2024-06-28 18:55:54 -0500

    side-bar 5

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -7,25 +7,25 @@
       </a>
     </li>
     <li class="nav-item">
-      <a href="/modelos-de-bd-primer-parcial/modules/users/" class="nav-link text-white d-flex">
+      <a href="http://localhost/modelos-de-bd-primer-parcial/modules/users/index.php" class="nav-link text-white d-flex">
         <div class="icon me-2"><i class="bi bi-person"></i></div>
         <p>Usuarios</p>
       </a>
     </li>
     <li class="nav-item">
-      <a href="/modelos-de-bd-primer-parcial/modules/clients/" class="nav-link text-white d-flex">
+      <a href="http://localhost/modelos-de-bd-primer-parcial/modules/clients/index.php" class="nav-link text-white d-flex">
         <div class="icon me-2"><i class="bi bi-people"></i></div>
         <p>Clientes</p>
       </a>
     </li>
     <li class="nav-item">
-      <a href="/modelos-de-bd-primer-parcial/modules/roles/" class="nav-link text-white d-flex">
+      <a href="http://localhost/modelos-de-bd-primer-parcial/modules/roles/index.php" class="nav-link text-white d-flex">
         <div class="icon me-2"><i class="bi bi-key"></i></div>
         <p>Roles</p>
       </a>
     </li>
     <li class="nav-item">
-      <a href="/modelos-de-bd-primer-parcial/modules/events/" class="nav-link text-white d-flex">
+      <a href="http://localhost/modelos-de-bd-primer-parcial/modules/events/index.php" class="nav-link text-white d-flex">
         <div class="icon me-2"><i class="bi bi-calendar-event"></i></div>
         <p>Reservaciones</p>
       </a>
