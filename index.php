700c09617002d661b38ac7e2e10697ed208338c2
sarai <113812473+SARAI0101@users.noreply.github.com>
2024-07-24 19:20:40 -0500

    act

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="es">
 <link rel="stylesheet" href="css/styles.css">
 <head>
     <meta charset="utf-8" />
@@ -10,8 +10,6 @@
       require_once 'includes/events.php';
     ?>
     <!-- Bootstrap CSS v5.2.1 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"/>
-
     <script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.0/locales-all.js"></script>
 
